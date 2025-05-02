1a63137f057522276355b9202cc7b102adbc10a6
Guilherme Caixeta <guilhermecaixeta19@gmai.com>
2025-05-02 17:00:16 -0300

    Atualização do css do sistema

--- templates/static/salas/js/script.js
+++ templates/static/salas/js/script.js
@@ -1,10 +1,11 @@
 function toggleMenu() {
-    const menu = document.getElementById("menu");
-    const display = window.getComputedStyle(menu).display;
-
-    if (display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('active');
+    
+    // Fechar menu ao clicar em um link (opcional)
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            navbar.classList.remove('active');
+        });
+    });
 }
